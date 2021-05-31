4217115efa2a6a42b4d6af0544ed0042a302bdd0
Lucas Öhrn <lucasohrn@gmail.com>
2021-05-31 14:19:07 +0200

    Add project files.

---
+++ WineStarIOS/WineStarIOS/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using WineStarIOS.ViewModels;
+using Xamarin.Forms;
+
+namespace WineStarIOS.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ WineStarIOS/WineStarIOS/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WineStarIOS.Models;
+using WineStarIOS.ViewModels;
+using WineStarIOS.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WineStarIOS.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
